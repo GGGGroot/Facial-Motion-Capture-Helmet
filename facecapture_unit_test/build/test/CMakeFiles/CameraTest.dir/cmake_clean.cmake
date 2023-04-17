4f33241c94333d003c198deeb02a7664bb5eee34
dorris-dsj <130188508+dorris-dsj@users.noreply.github.com>
2023-04-17 23:56:40 +0100

    Add files via upload

---
+++ facecapture_unit_test/build/test/CMakeFiles/CameraTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CameraTest.dir/test.cpp.o"
+  "CameraTest"
+  "CameraTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CameraTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ facecapture_unit_test/build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/raspberry/facecapture_unit_test/test
+# Build directory: /home/raspberry/facecapture_unit_test/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(CameraTest "/home/raspberry/facecapture_unit_test/build/test/CameraTest")
+set_tests_properties(CameraTest PROPERTIES  _BACKTRACE_TRIPLES "/home/raspberry/facecapture_unit_test/test/CMakeLists.txt;42;add_test;/home/raspberry/facecapture_unit_test/test/CMakeLists.txt;0;")
